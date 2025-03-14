67b6ec365518da43e12fda5f8dd3398009c83748
Shabbir2004 <shabbirabbas2004@gmail.com>
2025-03-14 15:27:39 +0530

    adding cchanged files

--- app.js
+++ app.js
@@ -6,13 +6,28 @@ const db = require("./config/mongooseConnection.js");
 const ownersRouter = require("./routes/ownersRouter.js");
 const usersRouter = require("./routes/usersRouter.js");
 const productsRouter = require("./routes/productsRouter.js");
+const index = require("./routes/index.js")
+const session = require("express-session");
+const flash = require("connect-flash");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET || "yourSecretKey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(flash());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
+app.use("/",index)
 app.use("/owners", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  logout,
+} = require("../controllers/authController.js");
 
-router.get("/", (req, res) => {
-  res.send("user is working");
-});
+require("dotenv").config();
+
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/logout", logout);
 
 module.exports = router;
