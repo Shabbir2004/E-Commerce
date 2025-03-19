a7a78b1a070ce07893fa73fdba6136aabea3b27d
Shabbir2004 <shabbirabbas2004@gmail.com>
2025-03-19 20:13:52 +0530

    shopping files added

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn.js");
+const productModel = require("../models/productmodel.js");
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
   res.render("index", { error });
 });
 
-router.get("/shop", isLoggedIn, (req, res) => {
-  res.render("try");
+router.get("/shop", isLoggedIn, async (req, res) => {
+  let products = await productModel.find();
+  res.render("shop" , {products});
 });
 
 module.exports = router;
