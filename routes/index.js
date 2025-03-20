f91d576759f38bb1e8f6045509db85a51aaee7a0
Shabbir2004 <shabbirabbas2004@gmail.com>
2025-03-21 00:11:38 +0530

    cart files added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middlewares/isLoggedIn.js");
 const productModel = require("../models/productmodel.js");
+const usermodel = require("../models/usermodel.js");
 
 router.get("/", (req, res) => {
   let error = req.flash("error");
@@ -10,7 +11,28 @@ router.get("/", (req, res) => {
 
 router.get("/shop", isLoggedIn, async (req, res) => {
   let products = await productModel.find();
-  res.render("shop" , {products});
+  let success = req.flash("succes");
+  res.render("shop", { products, success });
+});
+
+router.get("/cart", isLoggedIn, async (req, res) => {
+  let user = await usermodel
+    .findOne({ email: req.user.email })
+    .populate("cart");
+
+     const bill = (Number(user.cart[0].price)+50)-Number(user.cart[0].discount)
+
+  res.render("cart", { user , bill});
+});
+
+router.get("/addtocart/:id", isLoggedIn, async (req, res) => {
+  let user = await usermodel.findOne({ email: req.user.email });
+  
+
+  user.cart.push(req.params.id);
+  await user.save();
+  req.flash("Success", "Added to cart");
+  res.redirect("/shop");
 });
 
 module.exports = router;
